e2b58a5f4b084eb96902f450e1a961c137c3b67e
limacil <rzhanov.aka1@gmail.com>
2025-10-16 14:18:12 +0000

    less_1

---
+++ lab_rab_1/task_4.py
@@ -0,0 +1,8 @@
+a = [1, 5, "Good", "Bad"]
+b = [9, "Blue", "Red", 11] # Условие задачи
+
+print("1" + "Red") # 1
+print("5" + "Blue") # 2
+print(a[0] + b[0]) # 3
+print(a[1] ** b[3]) # 4
+print(a + b) # 5
