bcc64ec542b177bd5cbb9e044b7ccc8516ff5ac0
limacil <rzhanov.aka1@gmail.com>
2025-10-16 16:44:52 +0000

    less

---
+++ lec_if.py
@@ -0,0 +1,13 @@
+if 1:
+    print('Hello 1!')
+
+if 0:
+    print('Hello 0!')
+
+a = int(input('Enter: '))
+if a > 1:
+    print(f'Hello {a}!')
+
+b = 5
+if b == 5:
+    print(f'Hello {b}!')
---
+++ lec_log_oper.py
@@ -0,0 +1,8 @@
+a, b, c = 3, 4, 5
+
+if a > 4 and b == 2:
+    print('Good')
+elif b > 3 or c == 5:
+    print('Best')
+else:
+    print('Bad')
