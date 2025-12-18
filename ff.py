1a64caf606d5eb37c43b7f5b34f7706ed83b686d
limacil <rzhanov.aka1@gmail.com>
2025-12-18 16:21:32 +0000

    hh

--- ff.py
+++ ff.py
@@ -3,32 +3,28 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 
-def circle_move(R, angle_vel, time):
-    alpha = angle_vel * np.pi / 180 * time
-    x = R * np.cos(alpha)
-    y = R * np.sin(alpha)
+def circle_move(R, vx0, vy0, time):
+    x0 = vx0 * time
+    y0 = vy0 * time
+    alpha = np.arange(0, 2*np.pi, 0.1)
+    x = x0 + R*np.cos(alpha)
+    y = y0 + R*np.sin(alpha)
     return x, y
 
 
-fig, ax = plt.subplots ()
-ball = plt.plot([], [], 'o', color='r', label='Ball')
-ball_line, = plt.plot([], [], '-', color='r', label='Ball')
-
-frames = 180
-coords = np.zeros((frames, 2))
+fig, ax = plt.subplots()
+ball, = plt.plot([], [], 'o', color='r', label='Ball')
 
 
 def animate(i):
-    coords[i] = circle_move(R=2, angle_vel=1, time=i)
-    ball.set_data([coords[i][0]], [coords[i][1]])
-    ball_line.set_data(coords[:i, 0], coords[:i, 1])
-    return ball, ball_line
-
+    ball.set_data(circle_move(R=0.5, vx0=0.01, vy0=0.01, time=i))
+    return ball
+   
 
 edge = 3
 plt.axis('equal')
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
-ani = FuncAnimation(fig, animate, frames=frames, interval=30)
-ani.save('animation_2.gif', writer="pillow")
+ani = FuncAnimation(fig, animate, frames=100, interval=30)
+ani.save('animation_3.gif', writer="pillow")
