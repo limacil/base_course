598aa980bea738f6b35fa1d14f4f43452ddcd323
limacil <rzhanov.aka1@gmail.com>
2026-03-26 17:12:37 +0000

    gg

---
+++ kod_5.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def circle_plotter():
+    
+    alpha = 45
+    v_0 = 10
+
+    
+    x = np.arange(0, 5, 0.1)
+    y = x * np.tan(alpha) - (9.8 * x ** 2) / (2 * v_0 ** 2 * np.cos(alpha) ** 2)
+
+    plt.plot(x, y)
+    plt.axis('equal')
+    plt.savefig('kod_5.png')
+
+
+if __name__ == '__main__':
+    circle_plotter()
